74a3421977788001dd56467e4d6acf925201a063
Ramil <rikos111@yandex.ru>
2020-04-06 18:50:31 +0300

    Without API keys
    Signed-off-by: Ramil <rikos111@yandex.ru>

--- OCC_bot/Settings/Config.py
+++ OCC_bot/Settings/Config.py
@@ -1,8 +1,8 @@
 from OCC_bot.Settings.binance_api import Binance
 
 
-API_KEY = 'AIWpaNgtcDMM8LV8ZQkcnCEIsfgmHkVxJChftLlVbUroKvBLZfF6dOSHd8isGLXL',
-API_SECRET = 'u9khIRbhOb61eEdmjXRjTGYy8T7eFFNN4jsVTmkknsIDMAyCkdOPj8UiqRUH88XP'
+API_KEY = '',
+API_SECRET =''
 
 period = 14
 time_delay = 60
